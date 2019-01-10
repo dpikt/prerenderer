005b27b8e353d4e7fa99f659310c1d1d033c8501
David Pickart <davidstoreypickart@gmail.com>
2019-01-09 19:51:20 -0600

    Add recursive rendering POC

--- renderers/renderer-puppeteer/es6/renderer.js
+++ renderers/renderer-puppeteer/es6/renderer.js
@@ -1,11 +1,13 @@
 const promiseLimit = require('promise-limit')
 const puppeteer = require('puppeteer')
+const cheerio = require('cheerio')
 
 const waitForRender = function (options) {
   options = options || {}
 
   return new Promise((resolve, reject) => {
     // Render when an event fires on the document.
+    // setTimeout(() => resolve(), 000) // TODO
     if (options.renderAfterDocumentEvent) {
       if (window['__PRERENDER_STATUS'] && window['__PRERENDER_STATUS'].__DOCUMENT_EVENT_RESOLVED) resolve()
       document.addEventListener(options.renderAfterDocumentEvent, () => resolve())
@@ -77,28 +79,30 @@ class PuppeteerRenderer {
     const options = this._rendererOptions
 
     const limiter = promiseLimit(this._rendererOptions.maxConcurrentRoutes)
-
-    const pagePromises = Promise.all(
-      routes.map(
-        (route, index) => limiter(
+    const self = this
+    return new Promise(resolve => {
+      const pages = []
+      const pagePromises = {}
+      function doTheThing (route) {
+        return limiter(
           async () => {
-            const page = await this._puppeteer.newPage()
-
+            const page = await self._puppeteer.newPage()
+      
             if (options.consoleHandler) {
               page.on('console', message => options.consoleHandler(route, message))
             }
-
+      
             if (options.inject) {
               await page.evaluateOnNewDocument(`(function () { window['${options.injectProperty}'] = ${JSON.stringify(options.inject)}; })();`)
             }
-
+      
             const baseURL = `http://localhost:${rootOptions.server.port}`
-
+      
             // Allow setting viewport widths and such.
             if (options.viewport) await page.setViewport(options.viewport)
-
-            await this.handleRequestInterception(page, baseURL)
-
+      
+            await self.handleRequestInterception(page, baseURL)
+      
             // Hack just in-case the document event fires before our main listener is added.
             if (options.renderAfterDocumentEvent) {
               page.evaluateOnNewDocument(function (options) {
@@ -106,34 +110,52 @@ class PuppeteerRenderer {
                 document.addEventListener(options.renderAfterDocumentEvent, () => {
                   window['__PRERENDER_STATUS'].__DOCUMENT_EVENT_RESOLVED = true
                 })
-              }, this._rendererOptions)
+              }, self._rendererOptions)
             }
             
             const navigationOptions = (options.navigationOptions) ? { waituntil: 'networkidle0', ...options.navigationOptions } : { waituntil: 'networkidle0' };
             await page.goto(`${baseURL}${route}`, navigationOptions);
-
+      
             // Wait for some specific element exists
-            const { renderAfterElementExists } = this._rendererOptions
+            const { renderAfterElementExists } = self._rendererOptions
             if (renderAfterElementExists && typeof renderAfterElementExists === 'string') {
               await page.waitForSelector(renderAfterElementExists)
             }
-            // Once this completes, it's safe to capture the page contents.
-            await page.evaluate(waitForRender, this._rendererOptions)
-
+            // Once self completes, it's safe to capture the page contents.
+            await page.evaluate(waitForRender, self._rendererOptions)
+            const html = await page.content()
             const result = {
               originalRoute: route,
               route: await page.evaluate('window.location.pathname'),
-              html: await page.content()
+              html
             }
-
+      
+            const $ = cheerio.load(html)
+            const newStuff = $('a').toArray()
+              .map(a => a.attribs.href)
+              .filter(Boolean)
+              .filter(href => href.startsWith('/'))
+              // .slice(0, 5)
+            
+            newStuff.forEach(q => {
+              if (!Object.keys(pagePromises).includes(q) && !pages.some(p => p.originalRoute === q)) pagePromises[q] = doTheThing(q)
+            })
+      
             await page.close()
-            return result
+            pages.push(result)
+            console.log('route rendered', result.originalRoute)
+
+            delete pagePromises[route]
+            if (Object.keys(pagePromises).length === 0) resolve(pages)
           }
         )
-      )
-    )
+      }
+      routes.forEach(route => {
+        pagePromises[route] = doTheThing(route)
+      })
+    })
 
-    return pagePromises
+    // return pagePromises
   }
 
   destroy () {
